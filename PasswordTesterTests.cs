f4d03a1e145d50a7ef5234f5afbb0c8107dfc157
TCGBelle <lumaknight@gmail.com>
2025-02-10 07:39:42 +0000

    Initial commit

---
+++ PasswordTesterTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+
+
+namespace std
+{
+    public class PasswordTesterTests
+    {
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnIsNull_WhenPasswordIsNull()
+        {
+            var result = Passwords.PasswordTester(null, 5, 25, true, true);
+            Assert.Equal(new List<errorType> { errorType.IsNull }, result);
+        }
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnBelowMinimum_WhenSizeIsZero()
+        {
+            var result = Passwords.PasswordTester("ValidPassword1", 0,25, true, true);
+            Assert.Contains(errorType.BelowMinimum, result);
+        }
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnBelowSize_WhenPasswordTooShort()
+        {
+            var result = Passwords.PasswordTester("Iv2", 5, 25, true, true);
+            Assert.Contains(errorType.BelowSize, result);
+        }
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnAboveMaximum_WhenMaxSizeIsTooLarge()
+        {
+            var result = Passwords.PasswordTester("ValidPassword1", 5, 300, true, true);
+            Assert.Contains(errorType.AboveMaximum, result);
+        }
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnAboveSize_WhenPasswordTooLong()
+        {
+            var result = Passwords.PasswordTester(new string('A', 30), 5,25, false, true);
+            Assert.Contains(errorType.AboveSize, result);
+        }
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnNoCapitals_WhenCapitalLetterRequired()
+        {
+            var result = Passwords.PasswordTester("password1", 5, 25, true, true);
+            Assert.Contains(errorType.NoCapitals, result);
+        }
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnNoDigits_WhenDigitRequired()
+        {
+            var result = Passwords.PasswordTester("Password", 5, 25, true, true);
+            Assert.Contains(errorType.NoDigits, result);
+        }
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnMultipleErrors_WhenMultipleRulesFail()
+        {
+            var result = Passwords.PasswordTester("abc", 5, 25, true, true);
+            var expectedErrors = new List<errorType> { errorType.BelowSize, errorType.NoCapitals, errorType.NoDigits };
+            Assert.Equal(expectedErrors, result);
+        }
+        [Fact]
+        [Trait("TestType", "PasswordTest")]
+        public void PasswordTester_ShouldReturnNone_WhenPasswordIsValid()
+        {
+            var result = Passwords.PasswordTester("ValidPassword1", 5, 25, true, true);
+            Assert.Equal(new List<errorType> { errorType.None }, result);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace std
+{
+    public enum errorType
+    {
+        None,
+        BelowMinimum,
+        AboveMaximum,
+        IsNull,
+        BelowSize,
+        AboveSize,
+        NoDigits,
+        NoCapitals
+    }
+    public static class Passwords
+    {
+
+        public static List<errorType> PasswordTester(string password, int minSize, int maxSize ,bool needDigit, bool needCapital)
+        {
+            List<errorType> errors = new List<errorType>();
+
+            if (password == null)
+            {
+                errors.Add(errorType.IsNull);
+                return errors;
+            }
+
+            if (minSize <= 0)
+                errors.Add(errorType.BelowMinimum);
+
+            if (maxSize >= 256)
+                errors.Add(errorType.AboveMaximum);
+
+            if (password.Length < minSize)
+                errors.Add(errorType.BelowSize);
+
+            if (password.Length > maxSize)
+                errors.Add(errorType.AboveSize);
+
+            if (needCapital && !password.Any(char.IsUpper))
+                errors.Add(errorType.NoCapitals);
+
+            if (needDigit && !password.Any(char.IsDigit))
+                errors.Add(errorType.NoDigits);
+
+            return errors.Count > 0 ? errors : new List<errorType> { errorType.None };
+        }
+    }
+}
